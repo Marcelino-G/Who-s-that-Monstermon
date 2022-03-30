7744a075785081546e876be31d8085395d3bd70f
Marcelino <93845376+xProtocall@users.noreply.github.com>
2022-03-30 10:33:44 -0700

    fetch/promise practice (better) .then

--- Who's that Pokemon.js
+++ Who's that Pokemon.js
@@ -16,39 +16,89 @@ function urlLoop(){
 urlLoop();
 
 function fetching() {
-   (urlsArray.map((url) => {
-      fetch (url)
-         .then (response => response.json())
-         .then (data => dataArray.push(data))
-         .then (dataA => createPokemon(dataA))
+   return promise = new Promise ((resolve,reject) => {
+
+      urlsArray.map((url) => {
+         fetch (url)
+            .then (response => (response.json()))
+            .then (data => { 
+               dataArray.push(data);
+               if(dataArray.length == 4){
+                  resolve(dataArray)
+                  }
+               })
       })
-   )
+   })  
 }
-fetching()
 
-function createPokemon (x){
-   let pokemonFigure = document.createElement("figure")
-   holder.append(pokemonFigure);
-   let pokemonImg = document.createElement("img")
-   let pokemonCaption = document.createElement("figcaption")
+function createPokemon(){
 
-   dataArray.map((x) => {
+   return promise = new Promise ((resolve, reject) => {
 
-      let srcAttribute = x.sprites.back_default;
-      let altAttribute = x.species.name
+      dataArray.map((x) => {
+         let pokemonFigure = document.createElement("figure")
+         holder.append(pokemonFigure);
+         let pokemonImg = document.createElement("img")
+         let pokemonCaption = document.createElement("figcaption")
+   
+         let srcAttribute = x.sprites.back_default;
+         let altAttribute = x.species.name
+   
+         pokemonImg.setAttribute("src", srcAttribute)
+         pokemonImg.setAttribute("alt", altAttribute)
+         pokemonFigure.append(pokemonImg)
+   
+         pokemonCaption.innerHTML = altAttribute
+         pokemonFigure.append(pokemonCaption)
 
-      pokemonImg.setAttribute("src", srcAttribute)
-      pokemonImg.setAttribute("alt", altAttribute)
-      pokemonFigure.append(pokemonImg)
+         let figs = document.getElementsByTagName("figure");
+         
+         if (figs.length == 4){
+            let figs = document.getElementsByTagName("figure");
+            resolve (figs)
+         }
+      }) 
 
-      pokemonCaption.innerHTML = altAttribute
-      pokemonFigure.append(pokemonCaption)
-   }) 
 
-   let m = document.getElementsByTagName("figure");
-   m[1].style.display = "none";
+
+
+
+
+   })
+
+
+
+   
+  
+}
+
+function grabFigures (x) {
+
+   console.log(x[3])
 
 }
+
+
+
+
+
+
+const myPromise = new Promise((resolve , reject) => {
+
+   resolve(fetching())
+})
+
+myPromise
+   .then (data => createPokemon())
+   .then (figs => grabFigures(figs))
+
+
+
+
+
+
+
+
 
 
 // async function fetchJSON (url) {
