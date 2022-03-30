a19ef4705fbc708d85dbe40b5bdc3af33d180477
Marcelino <93845376+xProtocall@users.noreply.github.com>
2022-03-29 20:14:35 -0700

    JavaScript mess. Practicing promises.

--- Who's that Pokemon.js
+++ Who's that Pokemon.js
@@ -1,43 +1,70 @@
-const picCapContainer = document.getElementById("pokemonPics")
-// let pics = document.createElement("img")
-
-// holder.append(pics)
+const holder = document.getElementById("holder");
+let urlsArray = []
+let dataArray = []
 
 function randomNumber(){
    return Math.floor(Math.random() * (386-1) + 1)
 }
 
-async function srcAltCollect () {
-
+function urlLoop(){
    for (let i = 0; i < 4; i++){
-   
       let newNum = randomNumber();
-      let url = `https://pokeapi.co/api/v2/pokemon/${newNum}`
-   
-      fetch(url)
-         .then (response => response.json())
-         .then (data => {
-            let pics = document.createElement("img")
-            let srcAttribute = data.sprites.back_default
-            let altAttribute = data.species.name
-            pics.setAttribute("src", srcAttribute)
-            pics.setAttribute("alt", altAttribute)
-            picCapContainer.append(pics)
-            let caption = document.createElement("figcaption")
-            caption.innerHTML = altAttribute
-            picCapContainer.append(caption)
-         });
+      randomUrl = `https://pokeapi.co/api/v2/pokemon/${newNum}`
+      urlsArray.push(randomUrl)
    }
+}
+urlLoop();
+
+function fetching() {
+   (urlsArray.map((url) => {
+      fetch (url)
+         .then (response => response.json())
+         .then (data => dataArray.push(data))
+         .then (dataA => createPokemon(dataA))
+      })
+   )
+}
+fetching()
+
+function createPokemon (x){
+   let pokemonFigure = document.createElement("figure")
+   holder.append(pokemonFigure);
+   let pokemonImg = document.createElement("img")
+   let pokemonCaption = document.createElement("figcaption")
+
+   dataArray.map((x) => {
+
+      let srcAttribute = x.sprites.back_default;
+      let altAttribute = x.species.name
+
+      pokemonImg.setAttribute("src", srcAttribute)
+      pokemonImg.setAttribute("alt", altAttribute)
+      pokemonFigure.append(pokemonImg)
+
+      pokemonCaption.innerHTML = altAttribute
+      pokemonFigure.append(pokemonCaption)
+   }) 
+
+   let m = document.getElementsByTagName("figure");
+   m[1].style.display = "none";
 
 }
 
 
-
-   let pokemon = document.getElementsByTagName("img")
-
-   
-
-
-
-
-
+// async function fetchJSON (url) {
+//    const response = await fetch(url, {
+//      headers: { accept: "application/json" }
+//    });
+ 
+//    return response.json();
+//  }
+ 
+//  async function fetching () {
+//    return Promise.all(urlsArray.map(fetchJSON));
+//  }
+ 
+//  (async () => {
+ 
+//  console.log(await fetching());
+ 
+//  })();
