a18730d658cab132251ce1d075db91af32f123ad
Marcelino <93845376+xProtocall@users.noreply.github.com>
2022-03-31 22:06:39 -0700

    Core functioning JavaScript down.
    Wrote the JavaScript to fetch the data, present it within the html, and began creating and editing the .addeventlistener().

--- Who's that Pokemon.js
+++ Who's that Pokemon.js
@@ -1,23 +1,24 @@
 const holder = document.getElementById("holder");
+const questions = document.getElementById("questions");
 let urlsArray = []
 let dataArray = []
 
-function randomNumber(){
-   return Math.floor(Math.random() * (386-1) + 1)
+function randomNumber(max,min){
+   return Math.floor(Math.random() * (max-min) + min)
 }
 
 function urlLoop(){
    for (let i = 0; i < 4; i++){
-      let newNum = randomNumber();
+      let newNum = randomNumber(386,1);
       randomUrl = `https://pokeapi.co/api/v2/pokemon/${newNum}`
       urlsArray.push(randomUrl)
    }
 }
-urlLoop();
 
 function fetching() {
-   return promise = new Promise ((resolve,reject) => {
 
+   return promise = new Promise ((resolve,reject) => {
+      
       urlsArray.map((url) => {
          fetch (url)
             .then (response => (response.json()))
@@ -51,46 +52,96 @@ function createPokemon(){
          pokemonCaption.innerHTML = altAttribute
          pokemonFigure.append(pokemonCaption)
 
-         let figs = document.getElementsByTagName("figure");
+         let imgs = document.getElementsByTagName("img");
          
-         if (figs.length == 4){
-            let figs = document.getElementsByTagName("figure");
-            resolve (figs)
+         if (imgs.length === 4){
+            imgs = document.getElementsByTagName("img");
+            resolve(Promise.all(imgs))
          }
       }) 
+   })
+}
 
+function grabRandomDataArray () {
 
+   return new Promise((resolve, reject) => {
+      let dataArrayRandomNum = randomNumber(4,0)
+      resolve (dataArrayRandomNum)
+   }) 
+}
 
+function randoData(x) {
 
+   return new Promise((resolve, reject) => {
+      resolve(dataArray[x])
+   })
+}
 
+function randoFigure(x){
 
+   return new Promise((resolve, reject) => {
+
+      resolve(x)
    })
 
 
-
-   
-  
 }
 
-function grabFigures (x) {
+function creatingHints(x){
 
-   console.log(x[3])
+   questions.innerHTML = `<p>one of my abilities is called ${x.abilities[0].ability.name}.</p>
+   <p>I am a ${x.types[0].type.name} type of Pokemon.</p>
+   <p>You can find me in PokeMon ${x.game_indices[0].version.name}</p> `
 
+
+   return new Promise((resolve, reject) => {
+      resolve (questions)
+   })
+}
+
+function clicks(x, y, z) {
+
+   return new Promise((resolve, reject) => {
+
+      for (let i = 0; i < x.length; i++){
+
+         x[i].addEventListener("click", () => {
+
+            if (x[i].getAttribute("src") === y.getAttribute("src") ){
+
+               console.log("corrrecto")
+               x[i].setAttribute("src", z[i].sprites.front_default)
+            } else {
+               console.log ("wrong")
+            }
+         })
+      }
+   })
+}
+
+function con(x) {
+
+   return new Promise((resolve, reject) => {
+
+      resolve (console.log(x));
+   })
+}
+
+async function gettingAndMakingData() {
+
+   const result = await fetching();
+   const imgsHtml = await createPokemon();
+   const d = await con(result);
+   const randomArray = await grabRandomDataArray();
+   const randomDataArray = await randoData(randomArray);
+   const randomImgArray = await randoFigure(imgsHtml[randomArray]);
+   const hintHtml = await creatingHints(randomDataArray); 
+   const clicked = await clicks(imgsHtml, randomImgArray, result)
 }
 
 
-
-
-
-
-const myPromise = new Promise((resolve , reject) => {
-
-   resolve(fetching())
-})
-
-myPromise
-   .then (data => createPokemon())
-   .then (figs => grabFigures(figs))
+urlLoop();
+gettingAndMakingData();
 
 
 
