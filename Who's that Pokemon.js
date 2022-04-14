58d069b836a0d743ea0f361a3fce7dde2318e56d
Marcelino <93845376+xProtocall@users.noreply.github.com>
2022-04-13 17:58:27 -0700

    99% done with comments added.
    Project does what it was intended to do. Comments were added and some code was cleaned up.

--- Who's that Pokemon.js
+++ Who's that Pokemon.js
@@ -1,3 +1,4 @@
+// creating the score that reflects in the html
 let correctTotalHtml = document.getElementById("totalCorrect")
 let questionTotalHtml = document.getElementById("totalQuestionsNumber")
 let correctTotal = 0;
@@ -5,28 +6,36 @@ let questionTotal = 0;
 correctTotalHtml.innerHTML = correctTotal;
 questionTotalHtml.innerHTML = questionTotal;
 
+// the caption and image choices to choose from (4)
 const figCaptions = document.getElementsByClassName("captionLineUp")
-const hints = document.getElementsByClassName("hint");
-
 const imgs = document.getElementsByClassName("imgLineUp");
 const imgsArray = Array.from(imgs)
+
+// 3 hints
+const hints = document.getElementsByClassName("hint");
+
+// an empty array for urls to be later stored in
 let urlsArray = []
 
+// ONE image and caption that is used for the popup div
 const correctImg = document.getElementById("correctImg")
 const correctCaption = document.getElementById("correctCaption")
 
+// the popup content (div, next button, and the text that changes whether you got it wrong or not)
 const popUpDiv = document.getElementById("popUpCorrect")
 const nextButton = document.getElementById("next");
 const popUpText = document.getElementById("popUpText")
 
-
-
-
+// returns a random number
 function randomNumber(max,min){
    return Math.floor(Math.random() * (max-min) + min)
 }
 
+// uses the randomNumber() function and then inserts that number 
+// to the end of the url which leads to a unique pokemon, and then 
+// pushing it into the urlsArray. (4 times)
 function urlLoop(){
+
    for (let i = 0; i < 4; i++){
       let newNum = randomNumber(386,1);
       randomUrl = `https://pokeapi.co/api/v2/pokemon/${newNum}`
@@ -34,24 +43,7 @@ function urlLoop(){
    }
 }
 
-function updateQuestionCount(x){
-
-   return new Promise ((resolve, reject) => {
-
-      if (x === "CORRECT!"){
-         correctTotal ++;
-         questionTotal ++;
-         correctTotalHtml.innerHTML = correctTotal;
-         questionTotalHtml.innerHTML = questionTotal;
-      } else{
-         questionTotal ++;
-         questionTotalHtml.innerHTML = questionTotal;
-      }
-
-
-   })
-}
-
+// this fetches the data from the 4 urls stored in urlsArray.
 async function fetching() {
 
    return Promise.all(urlsArray.map( async (url) => {
@@ -61,6 +53,8 @@ async function fetching() {
    )
 }
 
+// some of the fetched data is pulled and 
+// inserted into the html elements (pokemon back image and pokemon name).
 function createPokemon(x){
 
    return promise = new Promise ((resolve, reject) => {
@@ -74,6 +68,7 @@ function createPokemon(x){
    })
 }
 
+// a random number between 0-3 is made.
 function makeRandomArrayNumber () {
 
    return new Promise((resolve, reject) => {
@@ -82,6 +77,9 @@ function makeRandomArrayNumber () {
    }) 
 }
 
+// the random number made from makeRandomArrayNumber () 
+// is used to target a random specific fetched result from the
+// fetched array created from the fetching() function 
 function makeRandomJsonResultsNumber(x,y) {
 
    return new Promise((resolve, reject) => {
@@ -89,15 +87,20 @@ function makeRandomJsonResultsNumber(x,y) {
    })
 }
 
+// the SAME random number made using the makeRandomArrayNumber()
+// that was applied to the makeRandomJsonResultsNumber(x,y) is going to be 
+// used here. so the *random fetched array result (makeRandomJsonResultsNumber(x,y)) 
+// and *random img array result in this function match.
 function makeRandomImgNumber(x, y){
 
    return new Promise((resolve, reject) => {
       resolve(x[y])
    })
-
-
 }
 
+// data is pulled ONLY from the 
+// *random fetched array result (makeRandomJsonResultsNumber(x,y))
+// to create information to be inserted into the html.
 function creatingHints(x, y){
 
    x[0].innerHTML = y.abilities[0].ability.name.toUpperCase()
@@ -109,6 +112,9 @@ function creatingHints(x, y){
    })
 }
 
+// "click" event listener that triggers when a pokemon (img) is chosen. 
+// depending on whether it was the correct answer or not,
+// the text will be different inside of the now visible popUpDiv (originally display none).
 function choosingAnswer(x) {
 
    return new Promise((resolve, reject) => {
@@ -134,14 +140,11 @@ function choosingAnswer(x) {
    })
 }
 
-nextButton.addEventListener("click", () => {
-
-   
-   reset();
-   popUpDiv.classList.replace("d-block", "d-none");
-
-})
-
+// makes the popUpDiv display none to display block, revealing it.
+// the text inside of it is determined based on the answer chosen,
+// letting you know if it was correct or not. regardless, the correct 
+// image (front view) and caption is used. correct data is taken from
+// makeRandomJsonResultsNumber(x,y) result
 function popUp(x){
    
    correctImg.setAttribute("src", x.sprites.front_default)
@@ -150,35 +153,78 @@ function popUp(x){
    popUpDiv.classList.replace("d-none", "d-block");
 }
 
+// this updates the score count two different ways, 
+// depending on whether you answered correct or not.
+function updateQuestionCount(x){
+
+   return new Promise ((resolve, reject) => {
+
+      if (x === "CORRECT!"){
+         correctTotal ++;
+         questionTotal ++;
+         correctTotalHtml.innerHTML = correctTotal;
+         questionTotalHtml.innerHTML = questionTotal;
+      } else{
+         questionTotal ++;
+         questionTotalHtml.innerHTML = questionTotal;
+      }
+   })
+}
+
+// "next" button inside the popUpDiv in html.
+// when clicked, it runs the reset() function and makes the 
+// popUpDiv display none again. 
+nextButton.addEventListener("click", () => {
+
+   reset();
+   popUpDiv.classList.replace("d-block", "d-none");
+})
+
+// empties the urlsArray of the previous random fetched urls (4).
+// it then runs the same process again, obtaining 4 new random urls
+// and pushing them into the new empty urlsArray. These new urls
+// are then used with fetch and to create the content all over again.  
 function reset() {
+   
    urlsArray = [];
    urlLoop();
    theWholeProcess();
 }
 
 
-
 async function theWholeProcess() {
 
    const jsonResults = await fetching();
+
+   // creates an array of 2 HTMLcollections. 
+   // the first consists of images and the second figcaptions
    const imgsFigCaptionsHtml = await createPokemon(jsonResults);
+
+   // random number to be used to access ONE matching json result and image 
+   // from their own array.
    const randomArrayNumber = await makeRandomArrayNumber();
    const randomJsonResult = await makeRandomJsonResultsNumber(jsonResults, randomArrayNumber);
    const randomImg = await makeRandomImgNumber(imgsFigCaptionsHtml[0], randomArrayNumber);
-   const pHtmlHints = await creatingHints(hints, randomJsonResult); 
-   const answerText = await choosingAnswer(randomJsonResult)
-   const update = await updateQuestionCount(answerText);
-   // const d = await con(update);
    
+   // uses the ONE random fetched json result to create
+   // and insert html content.
+   const pHtmlHints = await creatingHints(hints, randomJsonResult); 
+
+   // uses the ONE random fetched json result to determine
+   // what the text inside the popUpDiv is going to be.
+   const answerText = await choosingAnswer(randomJsonResult)
+
+   // the score is updated depending on the outcome of the
+   // popUpDiv text content, which is determined by 
+   // which pokemon was chosen.
+   const update = await updateQuestionCount(answerText);
+
+   // const d = await con(imgsFigCaptionsHtml);
 }
 
 
 urlLoop();
 theWholeProcess();
-
-
-
-
 
 
 
